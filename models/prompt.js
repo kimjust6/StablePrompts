0f928aaa64a0a344467f29e6867b89b610f33761
Justin Kim <mletemps@hotmail.com>
2023-08-01 04:28:16 -0400

    added model for prompts

---
+++ models/prompt.js
@@ -0,0 +1,18 @@
+import { Schema, model, models } from "mongoose";
+
+const PromptSchema = new Schema({
+    creator: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+    prompt: {
+        type: String,
+        required: [true, "A prompt is required."],
+    },
+    tag: {
+        type: String,
+    },
+});
+
+// use the Prompt model, || or else create it using PromptSchema
+const Prompt = models.Prompt || model("Prompt", PromptSchema);
