d24761e635b99fb798c7c147e927e340085c8890
Justin Kim <mletemps@hotmail.com>
2023-08-01 13:22:21 -0400

    updated styling

--- components/Form.jsx
+++ components/Form.jsx
@@ -18,7 +18,7 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
 
             <form
                 onSubmit={handleSubmit}
-                className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
+                className="mt-10 w-full flex flex-col gap-7 glassmorphism"
             >
                 <label>
                     <span className="font-satoshi font-semibold text-base text-gray-700">
