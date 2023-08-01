28c91e64f7b3e8556b77f1fafa39b652332be1a6
Justin Kim <mletemps@hotmail.com>
2023-08-01 18:15:33 -0400

    initial profile commit

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,8 +1,30 @@
+import PromptCard from "./PromptCard";
 
-const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
+    console.log({ data });
+    return (
+        <section className="w-full ">
+            <h1 className="head_text text-left">
+                <span className="purple_gradient">{name} Profile</span>
+            </h1>
+            <p className="desc text-left">{desc}</p>
 
-export default Profile
+            <div className="mt-10 prompt_layout">
+                {data?.map((post) => (
+                    <PromptCard
+                        key={post._id}
+                        post={post}
+                        handleDelete={() => {
+                            handleDelete && handleDelete(post);
+                        }}
+                        handleEdit={() => {
+                            handleEdit && handleEdit(post);
+                        }}
+                    ></PromptCard>
+                ))}
+            </div>
+        </section>
+    );
+};
+
+export default Profile;
