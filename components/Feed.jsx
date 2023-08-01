101efba5ea4483259b06de05ce86ae49f2066ab2
Justin Kim <mletemps@hotmail.com>
2023-08-01 13:23:19 -0400

    initial feed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,44 @@
+"use client";
+import { useState, useEffect } from "react";
+
+import PromptCardList from "./PromptCardList";
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+    const [searchText, setSearchText] = useState("");
+    const [posts, setPosts] = useState([]);
+    const searchOnChange = (e) => {
+        setSearchText(e.target.value);
+    };
 
-export default Feed
+    // on first load, get all the posts
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const response = await fetch("/api/prompt");
+            const responsePosts = await response.json();
+            setPosts(responsePosts);
+        };
+
+        fetchPosts();
+    }, []);
+
+    return (
+        <section className="feed">
+            <form className="relative w-full flex-center">
+                <input
+                    type="text"
+                    placeholder="Search for a tag or email"
+                    value={searchText}
+                    onChange={searchOnChange}
+                    required
+                    className="search_input peer"
+                ></input>
+            </form>
+            <PromptCardList
+                data={posts}
+                handleTagClick={() => {}}
+            ></PromptCardList>
+        </section>
+    );
+};
+
+export default Feed;
