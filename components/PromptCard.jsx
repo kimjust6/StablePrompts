0462423634a4ea03521c3f9451487e2a2dd51138
Justin Kim <mletemps@hotmail.com>
2023-08-01 13:21:53 -0400

    added initial prompt cards and prompt card list

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,8 +1,32 @@
+"use client";
 
-const PromptCard = () => {
-  return (
-    <div>PromptCard</div>
-  )
-}
+import { useState } from "react";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { usePathname, useRouter } from "next/navigation";
 
-export default PromptCard
+const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+    return (
+        <div className="prompt_card">
+            <div className="flex justify-between items-start gap-5">
+                <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+                    <Image
+                        src={post.creator.image}
+                        alt="userImage"
+                        width={40}
+                        height={40}
+                        className="rounded-full object-contain"
+                    ></Image>
+                    <div className="rounded-full object-contain">
+                        <h3 className="font-satoshi font-semibold text-gray-900">
+                            {post.creator.fName} {post.creator.lName}
+                        </h3>
+                        <p className="font-inter text-sm text-gray-500">{post.creator.email}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default PromptCard;
