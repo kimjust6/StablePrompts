e070863da6f50eea025a6b572ac4372c25409a31
Justin Kim <mletemps@hotmail.com>
2023-08-02 12:49:00 -0400

    updated for some corner cases

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -25,7 +25,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             <div className="flex justify-between items-start gap-5">
                 <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
                     <Image
-                        src={post.creator.image}
+                        src={post?.creator?.image}
                         alt="userImage"
                         width={40}
                         height={40}
@@ -33,7 +33,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
                     ></Image>
                     <div className="rounded-full object-contain">
                         <h3 className="font-satoshi font-semibold text-gray-900">
-                            {post.creator.fName} {post.creator.lName}
+                            {post?.creator?.fName} {post?.creator?.lName}
                         </h3>
                         <p className="font-inter text-sm text-gray-500">{post.creator.email}</p>
                     </div>
