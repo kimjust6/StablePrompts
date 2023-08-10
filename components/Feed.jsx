d434e3d86fee2fcad68e1971739728760b8c20df
Justin Kim <mletemps@hotmail.com>
2023-08-10 15:21:35 -0400

    added extra call

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -31,6 +31,7 @@ const Feed = () => {
     // on first load, get all the posts
     useEffect(() => {
         const fetchPosts = async () => {
+            await fetch("/api/prompt");
             const response = await fetch("/api/prompt");
             const responsePosts = await response.json();
             setPosts(responsePosts);
