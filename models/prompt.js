3f536e839425e1db97352bb05b9e0f8b19ba3f39
Justin Kim <mletemps@hotmail.com>
2023-08-11 10:45:41 -0400

    reverted changes

--- models/prompt.js
+++ models/prompt.js
@@ -3,7 +3,7 @@ import { Schema, model, models } from "mongoose";
 const PromptSchema = new Schema({
     creator: {
         type: Schema.Types.ObjectId,
-        ref: "user",
+        ref: "User",
     },
     prompt: {
         type: String,
@@ -15,5 +15,5 @@ const PromptSchema = new Schema({
 });
 
 // use the Prompt model, || or else create it using PromptSchema
-const Prompt = models.Prompt || model("prompt", PromptSchema);
+const Prompt = models.Prompt || model("Prompt", PromptSchema);
 export default Prompt;
