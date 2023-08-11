b2c1fc014bd11c89690137377e481801288f3cdb
Justin Kim <mletemps@hotmail.com>
2023-08-11 17:33:36 -0400

    updated error message

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -32,6 +32,7 @@ const Feed = () => {
     useEffect(() => {
         const fetchPosts = async () => {
             // await fetch("/api/prompt");
+            await fetch("/api/prompt");
             const response = await fetch("/api/prompt");
             const responsePosts = await response.json();
             setPosts(responsePosts);
