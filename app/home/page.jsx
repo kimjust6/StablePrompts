29353496ca49c7db0018f6260821ae54d9adfbcf
Justin Kim <mletemps@hotmail.com>
2023-08-12 02:59:10 -0400

    fixed text on front end

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -10,8 +10,7 @@ const Home = () => {
                     <span className="purple_gradient"> Stable Diffusion Prompts</span>
                 </h1>
                 <p className="desc text-center">
-                    Login with Google to find and share prompts for stable diffusion to create your own AI generated
-                    images.
+                    Login with Google to find and share prompts for stable diffusion. Share your AI generated images!
                 </p>
                 <Feed></Feed>
             </section>
