e5a23f8bab4175147649a23731bc7b49f6a90e39
Justin Kim <mletemps@hotmail.com>
2023-08-12 22:16:56 -0400

    removed duplicate call

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -34,7 +34,6 @@ const Feed = () => {
     useEffect(() => {
         const fetchPosts = async () => {
             try {
-                await fetch("/api/prompt");
                 const response = await fetch("/api/prompt");
                 const responsePosts = await response.json();
                 setAllPosts(responsePosts);
