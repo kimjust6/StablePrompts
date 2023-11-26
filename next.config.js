ff3ad28ff6a6ec4599cd4d91f550c7da466172df
Justin Kim <mletemps@hotmail.com>
2023-11-26 11:11:37 -0500

    updated next.js and tsconfig page

--- next.config.js
+++ next.config.js
@@ -2,7 +2,16 @@
 const nextConfig = {
     experimental: {},
     images: {
-        domains: ["lh3.googleusercontent.com"],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'i.imgur.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+            },
+        ],
     },
     webpack(config) {
         config.experiments = {
