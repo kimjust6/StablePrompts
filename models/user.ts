a1bc897b791316fecde66ee7433d7dae606efe3e
Justin Kim <mletemps@hotmail.com>
2023-11-28 01:28:27 -0500

    minor styling fix and reverted next auth version

--- models/user.ts
+++ models/user.ts
@@ -21,5 +21,5 @@ const UserSchema = new Schema({
 });
 
 // check if models.User exists, || if not create model User with UserSchema
-const User = models.user2 || model("user2", UserSchema);
+const User = models.user || model("user", UserSchema);
 export default User;
