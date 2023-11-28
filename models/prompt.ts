8e1ecd44e087359287e8ca1070a9f238fdd242f7
Justin Kim <mletemps@hotmail.com>
2023-11-28 01:47:37 -0500

    main

--- models/prompt.ts
+++ models/prompt.ts
@@ -1,19 +1,19 @@
 import { Schema, model, models } from "mongoose";
 
 const PromptSchema = new Schema({
-    creator: {
-        type: Schema.Types.ObjectId,
-        ref: "user2",
-    },
-    prompt: {
-        type: String,
-        required: [true, "A prompt is required."],
-    },
-    tag: {
-        type: String,
-    },
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: "user2",
+  },
+  prompt: {
+    type: String,
+    required: [true, "A prompt is required."],
+  },
+  tag: {
+    type: String,
+  },
 });
 
 // use the Prompt model, || or else create it using PromptSchema
-const Prompt = models.prompt2 || model("prompt2", PromptSchema);
+const Prompt = models.prompt2 || model("prompt", PromptSchema);
 export default Prompt;
