de77b7b373b123ddb8975d1c59bf575ffba9cb1d
Justin Kim <mletemps@hotmail.com>
2023-11-29 14:35:43 -0500

    inital revision of image generation working

--- app/homepage/url/page.tsx
+++ app/homepage/url/page.tsx
@@ -1,15 +1,20 @@
 "use client";
-
+import Loading from "@/components/Loading";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
+  generateImage,
   getStableDiffusionAPIMongoDB,
   setStableDiffusionAPIMongoDB,
 } from "@/utils/actions";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
 const SetUrl = () => {
   const [Url, setUrl] = useState<string>("");
+  const [promptText, setPromptText] = useState<string>("");
+  const [myImage, setMyImage] = useState("https://i.imgur.com/zvsrgSF.jpeg");
+  // const [isGeneratingImage, setIsGeneratingImage] = useState<boolean>(false);
 
   useEffect(() => {
     const getAPI = async () => {
@@ -19,25 +24,55 @@ const SetUrl = () => {
         setUrl(parsedUrl);
       }
     };
-
     getAPI();
   }, []);
   return (
-    <section className="mt-20 w-96 flex justify-center items-center gap-4">
-      <Input
-        type="text"
-        placeholder="Input to override the stable diffusion API"
-        value={Url}
-        onChange={(event) => {
-          setUrl(event.target.value);
-        }}
-      />
-      <Button
-        onClick={() => {
+    <section className="mt-20 flex flex-col justify-center items-center gap-4">
+      <form
+        className="flex gap-4 w-96"
+        onSubmit={async (event) => {
+          event.preventDefault();
           setStableDiffusionAPIMongoDB(Url);
         }}>
-        Submit
-      </Button>
+        <Input
+          type="text"
+          placeholder="Input to override the stable diffusion API"
+          value={Url}
+          onChange={(event) => {
+            setUrl(event.target.value);
+          }}
+        />
+        <Button>Submit</Button>
+      </form>
+      <form
+        className="flex gap-4 w-96"
+        onSubmit={async (event) => {
+          event.preventDefault();
+          setMyImage(null);
+          const response = await generateImage(promptText);
+          setMyImage(response);
+        }}>
+        <Input
+          type="text"
+          value={promptText}
+          onChange={(event) => {
+            setPromptText(event.target.value);
+          }}
+          placeholder="input text to generate image from stable diffusion API"
+        />
+        <Button type="submit">Generate Image</Button>
+      </form>
+
+      {myImage ? (
+        <Image
+          alt="ai generated image"
+          width="512"
+          height="512"
+          src={`data:image/png;base64,${myImage}`}
+        />
+      ) : (
+        <Loading />
+      )}
     </section>
   );
 };
--- utils/actions.ts
+++ utils/actions.ts
@@ -92,3 +92,75 @@ export async function getUserById(id: string) {
     return JSON.stringify({ error: "User does not exist." });
   }
 }
+
+export async function generateImage(prompt: string) {
+  try {
+    // body for the stable diffusions api call
+    const data = {
+      prompt: prompt,
+      negative_prompt: "",
+      styles: ["string"],
+      seed: -1,
+      subseed: -1,
+      subseed_strength: 0,
+      seed_resize_from_h: -1,
+      seed_resize_from_w: -1,
+      sampler_name: "Euler a",
+      batch_size: 1,
+      n_iter: 1,
+      steps: 25,
+      cfg_scale: 7,
+      width: 512,
+      height: 512,
+      restore_faces: true,
+      tiling: true,
+      do_not_save_samples: false,
+      do_not_save_grid: false,
+      eta: 0,
+      denoising_strength: 0,
+      s_min_uncond: 0,
+      s_churn: 0,
+      s_tmax: 0,
+      s_tmin: 0,
+      s_noise: 0,
+      override_settings: {},
+      override_settings_restore_afterwards: true,
+
+      refiner_switch_at: 0,
+      disable_extra_networks: false,
+      comments: {},
+      enable_hr: false,
+      firstphase_width: 0,
+      firstphase_height: 0,
+      hr_scale: 2,
+      hr_second_pass_steps: 0,
+      hr_resize_x: 0,
+      hr_resize_y: 0,
+      hr_prompt: "",
+      hr_negative_prompt: "",
+      sampler_index: "Euler",
+      script_name: "",
+      script_args: [],
+      send_images: true,
+      save_images: false,
+      alwayson_scripts: {},
+    };
+
+    await connectToDB();
+    // get url for stable diffusions api
+    const base_url = await StableAPI.findOne({});
+
+    // make the stable dffusion api call
+    const response = await fetch(`${base_url.url}/sdapi/v1/txt2img`, {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      headers: new Headers({ "content-type": "application/json" }),
+      mode: "no-cors",
+      body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
+    const image = await response.json(); // parses JSON response into native JavaScript objects
+    return image.images[0];
+  } catch (error) {
+    console.log(error);
+    return JSON.stringify({ error: "User does not exist." });
+  }
+}
